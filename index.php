d0228dc04e1bcfbf878129c7dadb8cda3124b57d
wira07 <wiralodrasaputra07@gmail.com>
2023-12-18 10:40:55 +0700

    initial commit for php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $nama = "Wira Sukma Saputra";
+
+    $nama_depan = "Sarjana";
+    $nama_belakang = "S.kom";
+    // echo $nama_depan . " " . $nama_belakang;
+    echo "$nama_depan $nama_belakang";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wira Sukma Saputra</title>
+</head>
+<body>
+    <h1>Halo, Selamat datang <?php echo $nama; ?> </h1>
+</body>
+</html>
