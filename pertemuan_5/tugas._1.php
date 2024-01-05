c569ca7b385a2865664e55d73240108744133f66
Wira07 <wiralodrasaputra07@gmail.com>
2024-01-05 22:52:04 +0700

    Update style.css and tugas._1.php

--- pertemuan_5/tugas._1.php
+++ pertemuan_5/tugas._1.php
@@ -11,13 +11,48 @@
 
 <body>
 
+    <!-- Container -->
     <div class="container d-flex justify-content-center align-items-center min-vh-100">
         <div class="row border rounder-5 p-3 bg-white shadow box-area">
-            <div class="col-md-6 left-box">
-                <div class="featured-image">
-                    <img src="../pertemuan_5/images/1.png" alt="Wira Sukma Saputra" class="image-fluid" style="width: 250px;">
-                    <p>Be Verivied</p>
-                    <small>Wira Sukma Saputra</small>
+            <!-- Left -->
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-item-center flex-column-end left-box" style="background: #103cbe;">
+                <div class="featured-image mb-3">
+                    <img src="../pertemuan_5/images/1.png" alt="Wira Sukma Saputra" class="image-fluid" style="width: 380px; margin-top: 20px;">
+                    <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: bold;">Be Verivied</p>
+                    <small class="text-white text-wrap text-center" style="width: 17rem;">Wira Sukma Saputra</small>
+                </div>
+            </div>
+            <!-- Right -->
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center p-4">
+                    <div class="header-text mb-4">
+                        <h2>Hello, Wira</h2>
+                        <p>We are great english speakers</p>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control-lg bg-light fs-6" style="width: 500px;" placeholder="Emaill Address">
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control-lg bg-light fs-6" style="width: 500px;" placeholder="Password">
+                    </div>
+                    <div class="input-group mb-6 d-flex justify-content-between">
+                        <div class="form-check ">
+                            <input type="checkbox" class="form-check-input" id="formCheck">
+                            <label for="formCheck" class="form-check-label text-secondary "><small>Remember Me</small></label>
+                        </div>
+                        <div class="forgot">
+                            <small><a href="#">Forgot Password</a></small>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button class="btn btn-lg btn-light w-100 fs-6"><img src="../pertemuan_5/images/google.png" alt="google" style="width: 20px;"><small class="me-2">Sign In With Google</small></button>
+                        </div>
+                        <div class="row">
+                            <small>Don't Have Account? <a href="#">Sign Up</a></small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
