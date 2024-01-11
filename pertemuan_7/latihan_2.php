f022e3f9ca10462486a1114252409c323e374edd
Wira07 <wiralodrasaputra07@gmail.com>
2024-01-11 17:29:22 +0700

    mengedit file

--- pertemuan_7/latihan_2.php
+++ pertemuan_7/latihan_2.php
@@ -1,27 +1,28 @@
 <?php
 $mahasiswa = [
     [
-        "nama" => "Rizky",
+        "nama" => "Wira Sukma Saputra",
         "nrp" => "173040034",
         "email" => "rizkymuh@gmail.com",
         "jurusan" => "Teknik Informatika",
         "gambar" => "Daftar Dosen.png"
     ],
     [
-        "nama" => "Rizky",
-        "nrp" => "173040034",
+        "nama" => "Agum Pratama",
+        "nrp" => "173041034",
         "email" => "rizkyyusuf98@gmail.com",
         "jurusan" => "Teknik Informatika",
         "gambar" => "Daftar Dosen.png"
     ],
     [
-        "nama" => "Rizky",
-        "nrp" => "173040034",
+        "nama" => "Neno Arisma",
+        "nrp" => "173040014",
         "email" => "rizkyrizki123@hotmail.com",
         "jurusan" => "Teknik Informatika",
         "gambar" => "Daftar Dosen.png"
     ],
 ];
+
 
 ?>
 
@@ -31,35 +32,21 @@ $mahasiswa = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wira Sukma Saputra</title>
 </head>
 
 <body>
     <?php foreach ($mahasiswa as $mhs) : ?>
+        <li><?= $mhs["nama"]; ?></li>
         <ul>
-            <li><img src="../pertemuan_7/gambar/<?= $mhs[" gambar"]; ?>"></li>
-            <li></li>
+            <li>
+                <a href="latihan_3.php?nama=<?= $mhs["nama"]; ?>&nrp=<?= $mhs["nrp"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+                <a href="latihan_3.php?nama=<?= $mhs["nama"]; ?>"></a>
+            </li>
         </ul>
     <?php endforeach; ?>
+
+
 </body>
 
-</html>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <li>php</li>
-        <li><?php ?></li>
-        <li><?php ?></li>
-        <li>php</li>
-    </ul>
-    
-</body>
 </html>
