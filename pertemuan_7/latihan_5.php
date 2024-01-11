9d46aea4973fbffdf1f745dcad56e7f0b6f1dd2c
Wira07 <wiralodrasaputra07@gmail.com>
2024-01-11 20:10:26 +0700

    update file

---
+++ pertemuan_7/latihan_5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Selamat Datang, <?= $_POST["nama"]; ?></h1>
+    <h2>nrp, <?= $_POST["nrp"]; ?></h2>
+    <h2>email, <?= $_POST["email"]; ?></h2>
+    <h2>jurusan, <?= $_POST["jurusan"]; ?></h2>
+    <h2>gambar, <?= $_POST["gambar"]; ?></h2>
+    <h2>fakultas, <?= $_POST["gambar"]; ?></h2>
+    <h2>jenis_kelamin, <?= $_POST["jenis_kelamin"]; ?></h2>
+    <h2>alamat, <?= $_POST["alamat"]; ?></h2>
+    <h2>no_telepon, <?= $_POST["no_telepon"]; ?></h2>
+</body>
+
+</html>
