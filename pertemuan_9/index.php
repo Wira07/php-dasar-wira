2fe7fe4378e4d168e7e339b3bd492e1e8b54e802
Wira07 <wiralodrasaputra07@gmail.com>
2024-01-15 21:45:12 +0700

    update

--- pertemuan_9/index.php
+++ pertemuan_9/index.php
@@ -27,6 +27,42 @@
     // ambil data dari table mahasiswa
     $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
 
+    ?>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Email</th>
+            <th>Alamat</th>
+            <th>Email</th>
+        </tr>
+        <!-- looping untuk setiap baris data -->
+        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+                <td>1</td>
+                <td>
+                    <a href="">Edit</a> |
+                    <a href="">Delete</a>
+                </td>
+                <td><img src="../pertemuan_9/gambar/<?php echo $row["Gambar"]; ?>" width="50"></td>
+                <td><?= $row["Nama"] ?></td>
+                <td><?= $row["Nim"] ?></td>
+                <td><?= $row["Email"] ?></td>
+                <td><?= $row["Alamat"] ?></td>
+                <td><?= $row["Prodi"] ?></td>
+
+            </tr>
+        <?php endwhile; ?>
+    </table>
+
+    <?php
     // if (!$result) {
     //     echo mysqli_error($conn);
     // }
@@ -40,37 +76,6 @@
     //     var_dump($mhs);
     // }
     ?>
-</head>
-
-<body>
-    <h1>Daftar Mahasiswa</h1>
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No.</th>
-            <th>Aksi</th>
-            <th>Gambar</th>
-            <th>NIM</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Jurusan</th>
-        </tr>
-        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
-            <tr>
-                <td>1</td>
-                <td>
-                    <a href="">Edit</a> |
-                    <a href="">Delete</a>
-                </td>
-                
-                <td><img src="../pertemuan_9/gambar/<?php echo $row["Gambar"]; ?>" width="50"></td>
-
-                <td>201401001</td>
-                <td>Wira Sukma Saputra</td>
-                <td>wiralodrasaputra07@gmail.com</td>
-                <td>Teknik Informatika</td>
-            </tr>
-        <?php endwhile; ?>
-    </table>
 </body>
 
 </html>
